bf06deefa22c09a09c37ab70dd3ab6f6fff17065
dvsilkov <dvsilkov@gmail.com>
2025-09-24 23:43:53 +0300

    Параметризация Pytest. Практика работы с pytest параметризацией

--- tests/test_parametrization.py
+++ tests/test_parametrization.py
@@ -46,3 +46,19 @@ class TestOperations:
     def test_user_without_operations(self, user: str):
         print(f"User without operations: {user}")
 # command to run - python -m pytest -k "TestOperations" -s -v
+
+# работа с идентификаторами
+# Словарь пользователей: номер телефона — ключ, описание — значение
+users = {
+    "+70000000011": "User with money on bank account",
+    "+70000000022": "User without money on bank account",
+    "+70000000033": "User with operations on bank account"
+}
+
+@pytest.mark.parametrize(
+    "phone_number",
+    users.keys(),  # Передаем список номеров телефонов
+    ids=lambda phone_number: f"{phone_number}: {users[phone_number]}"  # Генерируем идентификаторы динамически
+)
+def test_identifiers(phone_number: str):
+    pass
